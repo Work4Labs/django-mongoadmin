a888623b3a42628e63b2babf83bbfcd2f27709e4
Olivier Le Floch <olivier@le-floch.fr>
2015-03-09 22:19:36 -0700

    Fix for: object has no attribute '_deferred' This is most likely not the right way to do this.:

--- mongoadmin/sites.py
+++ mongoadmin/sites.py
@@ -53,6 +53,8 @@ class MongoAdminSite(AdminSite):
         for model in model_or_iterable:
             if isinstance(model, TopLevelDocumentMetaclass):
                 init_document_options(model)
+                # FIXME: Fix for Django 1.7+, probably not the right way to do this
+                model._deferred = False
             
             if hasattr(model._meta, 'abstract') and model._meta.abstract:
                 raise ImproperlyConfigured('The model %s is abstract, so it '
